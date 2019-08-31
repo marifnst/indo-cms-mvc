fd82f6f75cac3269bdbda4c6501314b2a47d55cc
marifnst <arif.nasution@emeriocorp.com>
2019-08-31 15:33:27 +0700

    init indo-cms-mvc

---
+++ src/main/java/com/indocms/mvcapp/controller/AuthController.java
@@ -0,0 +1,111 @@
+package com.indocms.mvcapp.controller;
+
+import com.indocms.mvcapp.model.LoginModel;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class AuthController {
+    @GetMapping("/")
+    public ModelAndView defaultPage(@ModelAttribute("login_model_attribute") LoginModel loginModel) {
+        ModelAndView output = new ModelAndView();
+        System.out.println(loginModel.getLoginMessage());
+        if (loginModel != null && loginModel.getLoginMessage() != null) {
+            System.out.println("loginModel.getLoginMessage() : " + loginModel.getLoginMessage());
+        }
+
+        output.setViewName("pages/login");
+        return output;
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ModelAndView login(@ModelAttribute LoginModel loginModel, RedirectAttributes redirectAttributes) {
+        ModelAndView output = new ModelAndView();        
+        System.out.println(loginModel.getUsername() + " : " + loginModel.getPassword());
+        String username = loginModel.getUsername();
+        String password = loginModel.getPassword();
+
+        if (username.equals("admin@admin.com") && password.equals("admin")) {
+            output.setViewName("redirect:/home");
+        } else {
+            output.setViewName("redirect:/");
+            loginModel.setLoginMessage("Invalid Username or Password");
+            redirectAttributes.addFlashAttribute("login_model_attribute", loginModel);
+        }
+        return output;
+    }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "pages/register";
+    }
+
+    @RequestMapping(value = "/register/process", method = RequestMethod.POST)
+    public String registerProcess() {
+        return "redirect:/home";
+    }
+
+    @GetMapping("/home")
+    public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "home";
+    }
+
+    @GetMapping("/profile")
+    public String profilePage() {
+        return "pages/profile";
+    }
+
+    @GetMapping("/role")
+    public String rolePage() {
+        return "pages/role";
+    }
+
+    @GetMapping("/send/verified")
+    public String sendVerifiedPage() {
+        return "pages/send_verified";
+    }
+
+    @GetMapping("/verify/review")
+    public String verifyReviewPage() {
+        return "pages/verify_review";
+    }
+
+    @GetMapping("/score/review")
+    public String scoreReviewPage() {
+        return "pages/score_review";
+    }
+
+    @GetMapping("/dupak")
+    public String dupakPage() {
+        return "pages/dupak";
+    }
+
+
+    @GetMapping("/document")
+    public String documentPage() {
+        return "pages/document";
+    }
+
+    @GetMapping("/forms")
+    public String tes(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "pages/forms/general";
+    }
+
+    @GetMapping("/chartjs")
+    public String chartJs(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "pages/charts/chartjs";
+    }
+
+}
---
+++ src/main/java/com/indocms/mvcapp/model/LoginModel.java
@@ -0,0 +1,31 @@
+package com.indocms.mvcapp.model;
+
+public class LoginModel {
+    String username;
+    String password;
+    String loginMessage;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLoginMessage() {
+        return loginMessage;
+    }
+
+    public void setLoginMessage(String loginMessage) {
+        this.loginMessage = loginMessage;
+    }
+}
